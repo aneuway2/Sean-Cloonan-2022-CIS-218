62f41c7a8461a99b9346fe525324bf5c5bb1d30b
Sean <scloonan@mail.immaculata.edu>
2022-02-03 19:32:46 -0500

    resubmission

--- lab2.py
+++ lab2.py
@@ -1,21 +1,30 @@
-Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 def egg_size():
     """Sean Cloonan, https://foodchannel.com/2010/how-large-is-a-large-egg"""
-    x = float(input("Please enter an integer: "))
-    if x <= 0:
-        print('Egg must weigh more than zero')
-    elif  x > 0 and x <= 15:
-        print('Your egg size is Pee Wee')
-    elif  x > 15 and  x <= 18:
+    gram_to_ounce_factor = 1/28.35 # 1 ounce = 28.34952 gram (Approximating 28.34952 to 28.35)
+    while True:   
+        scale = input("Scale for weight of Eggs-\nGrams- g \nOunce - oz\n")
+        if scale.lower() in["g","oz"]:
+            break
+        print("Please enter appropriate scale.")
+    while True:
+        x = float(input("Enter weight of egg in the scale as selected: "))
+        if scale == "g":
+            x = float(x) * gram_to_ounce_factor
+        if x <= 0:
+            print('Egg must weigh more than zero')
+        elif x <= 15:
+             print('Your egg size is Pee Wee')
+        elif x <= 18:
             print('Your egg size is Small')
-    elif  x > 18 and x <= 21:
+        elif x <= 21:
             print('Your egg size is Medium')
-    elif  x > 21 and x <= 24:
+        elif x <= 24:
             print('Your egg size is Large') 
-    elif  x > 24 and x <= 27:
+        elif x <= 27:
             print('Your egg size is Extra Large')
-    elif  x > 27:
+        else:
             print('Your egg size is Jumbo')
-    else:
-        print('Please use a number')
+
+# Added by Andrew Bowman to enable running this lab with the terminal command:
+#   python lab2.py
+egg_size()
