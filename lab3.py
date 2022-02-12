ec21db4012c632f0c52d59e7b3c942cc7823de76
Sean <scloonan@mail.immaculata.edu>
2022-02-12 09:19:01 -0500

    revision

--- lab3.py
+++ lab3.py
@@ -1,20 +1,20 @@
-my_string = input()
-vowels = ["a", "e", "i", "o", "u"]
-upper_case_letters = lower_case_letters = spaces = digit = punctuation = vowel = 0
+my_string =  input("Type xxx here: ")
+vowels = ["a","e","i", "o", "u"]
+upper_case_letters,lower_case_letters,spaces,digit,punctuation,vowel =(0,0,0,0,0,0)
 for char in my_string:
-                if char.isalpha():
-             if char.isupper():
-           upper_case_letters +=1
-           if char.lower() in vowels:
-               vowel += 1
-       elif char.islower():
-           lower_case_letters += 1
-           if char in vowels:
-               vowel += 1
-   elif char.isdigit():
-       digit += 1
-   elif char == " ":
-       spaces += 1
-   else:
-       punctuation += 1
-print(f"Upper Case letters: {upper_case_letters}\nLower case letters: {lower_case_letters}\nSpaces: {spaces}\nDigits: {digit}\nPunctuation: {punctuation}\nVoels: {vowel}")ase letters: {lower_case_letters}\nSpaces: {spaces}\nDigits: {digit}\nPunctuation: {punctuation}\nVoels: {vowel}")
+    if(char.isalpha()):
+        if(char.isupper()):
+            upper_case_letters +=1
+            if char.lower() in vowels:
+                vowel += 1
+        elif (char.islower()):
+            lower_case_letters += 1
+            if char in vowels:
+                vowel += 1
+    elif (char.isdigit()):
+        digit += 1
+    elif char == " ":
+        spaces += 1
+    else:
+        punctuation += 1
+print(f"Upper Case letters: {upper_case_letters}\nLower case letters: {lower_case_letters}\nSpaces: {spaces}\nDigits: {digit}\nPunctuation: {punctuation}\nVoels: {vowel}")
