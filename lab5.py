f62967867ae459920b1964578ca514cd5614f291
Sean <scloonan@mail.immaculata.edu>
2022-03-01 00:43:26 -0500

    revision

--- lab5.py
+++ lab5.py
@@ -1,23 +1,19 @@
-Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15)
 [Clang 6.0 (clang-600.0.57)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> facts_temperature_celcius =  {0: "Freezing point of water.", 100: "Boiling point of water."}
 def convert_to_fahrenheit(celcius):
-    fahrenheit = ( celcius * 9/5) + 32
-    if celcius in facts_temperature_celcius:
-        return fahrenheit, facts_temperature_celcius[celcius]
-    else:
-        return fahrenheit
-    
-        
+    ''' The function converts temperature in degree Celsius  into temperature in
+    degree Fahrenheit. It also returns facts about the temperature.'''
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit, facts_temperature_celcius.get(celcius, "")
+
 def test():
-        celcius_list = [0, 25, 50, 100]
-        for celcius in celcius_list:
-            data = convert_to_fahrenheit(celcius)
-            if isinstance(data, tuple):
-                print(f'{celcius} degree Celcius = {data[0]} degree Fahrenheit.\n{celcius} degree Celsius is {data[1]}')
-            else:
-                print(f'{celcius} degree Celcius = {data} degree Fahrenheit.')
-            print()
-                
+    celcius_temp = float(input("Enter temperature in celsius: "))
+    data = convert_to_fahrenheit(celcius_temp)
+    if data[1]:
+        print(f'{celcius_temp} degree Celcius = {data[0]} degree Fahrenheit.\n{celcius_temp} degrees Celsius is {data[1]}')
+    else:
+        print(f'{celcius_temp} degree Celcius = {data[0]} degree Fahrenheit.')
+
 test()
