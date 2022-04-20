c5708d458b75014f6212cc8cdcd97e3591102f70
Sean <scloonan@mail.immaculata.edu>
2022-04-19 22:56:29 -0400

    revision

--- lab11.py
+++ lab11.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 '''This package has one Goods class used to calculate tax amount.
    It has a Toys subclass of Goods class which sets the toy type. It is also testing
    the different functions implemented in the class.'''
 
 class Goods:
 
-    '''This is subclass of Goods. It takes three attributes of the toy - base price, perecnt sales tax and type of toy.
-    It is used to calculate the tax amount on different type of toys using its super class method get_tax_amount.
-    The toy could be used by a boy or a girl , default toy is used by boys.'''
+    '''It takes two attributes of the Goods - base price, perecnt sales tax .
+    It is used to calculate the tax amount on different type of goods.
+    '''
 
     def __init__(self, base_price, percent_sales_tax):
         """
@@ -18,14 +17,14 @@ class Goods:
      percent_sales_tax : float
           Percentage sales tax on the item
        """
-        self.base_price =  base_price
-        self.percent_sales_tax = percent_sales_tax
+        self.__base_price = base_price
+        self.__percent_sales_tax = percent_sales_tax
 
     def __str__(self):
-        return f"Base price is {self.base_price} and  percentage sales tax is {self.percent_sales_tax}"
+        return f"Base price is {self.__base_price} and  percentage sales tax is {self.__percent_sales_tax}"
 
     def __repr__(self):
-        return f'Goods({self.base_price}, {self.percent_sales_tax})'
+        return f'Goods({self.__base_price}, {self.__percent_sales_tax})'
 
     def __eq__(self, other):
         '''
@@ -35,7 +34,7 @@ class Goods:
         ----------
         Equality of the objects are compared on base price.
         '''
-        return self.base_price == other.base_price
+        return self.__base_price == other.__base_price
 
     def __lt__(self, other):
         '''
@@ -45,7 +44,7 @@ class Goods:
         ----------
         Two objects are compared on base price.
         '''
-        return self.base_price < other.base_price
+        return self.__base_price < other.__base_price
 
     def __ge__(self, other):
         '''
@@ -55,12 +54,12 @@ class Goods:
         ----------
         Two objects are compared on base price.
         '''
-        return self.base_price >= other.base_price
+        return self.__base_price >= other.__base_price
 
     def get_tax_amount(self):
         '''It calculates tax amount of an item if given base price and percentage tax on the item.'''
-        tax_amount =  self.base_price * self.percent_sales_tax/100
-        print(f'Base price is ${self.base_price} and Tax amount is ${tax_amount:.2f}')
+        tax_amount = self.__base_price * self.__percent_sales_tax/100
+        return tax_amount
 
 
 
@@ -89,6 +88,14 @@ class Toys(Goods):
 good1 = Goods(50, 5)
 
 
+def test_get_tax_amount():
+    """
+    For testing of calculation tax amount based on the private variables.
+    """
+    tax_amount = good1.get_tax_amount()
+    assert tax_amount == 2.5
+
+
 def test_str():
     '''For testing __str__ function'''
     string = good1.__str__()
@@ -112,4 +119,4 @@ def test_ge():
 def test_lt():
     ''' for testing __lt__ function'''
     good4 = Goods(60, 5)
-    assert good1 < good4
+assert good1 < good4
